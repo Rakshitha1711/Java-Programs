de82f9e7816b089fa3c0a4b13f8bd4f0c87b4d69
Rakshitha1711 <rakshithabg17@gmail.com>
2023-09-29 22:47:25 +0530

    Version-1

---
+++ Patterns/src/com/kodnest/patterns/Patterns.java
@@ -0,0 +1,26 @@
+package com.kodnest.patterns;
+
+public class Patterns {
+
+	public static void main(String[] args) 
+	{
+		for(int i=0;i<=5;i++)
+		{
+			for(int j=0;j<=i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=5;j>=i;j--)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
